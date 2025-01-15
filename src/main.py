1c6c901fdd3f89518fc6dd71751a68abfb23e4f7
Rodger// <85492925+JerryHong08@users.noreply.github.com>
2025-01-15 15:07:18 +0800

    before update candlestick

--- src/main.py
+++ src/main.py
@@ -2,11 +2,8 @@ import sys
 from PyQt5.QtWidgets import QApplication
 #from ui.main_window3 import MainWindow
 from ui.main_window import MainWindowUI
-from PyQt5.QtCore import Qt  # Add this import
 
 def main():
-    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
     app = QApplication(sys.argv)
     window = MainWindowUI()
     window.show()
